cd22e0ceafe70f1e86d69d8acf9718f68130bc88
Bianca Medina <122565445+medinajk@users.noreply.github.com>
2024-11-04 14:22:37 -0300

    Prática de Arrays

---
+++ exArrays1/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+
+
+        int[] vect = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("NUMEROS NEGATIVOS: ");
+
+        for (int i = 0; i < n; i++) {
+            if (vect[i] < 0){
+                System.out.printf("%d\n", vect[i]);
+        }
+    }
+
+            sc.close();
+}
+}
